d55a97d259e781e257ca0b4256c403619d55dc69
苍穹勇敢的心 <18825237023@163.com>
2019-11-21 18:02:36 +0800

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,10 @@
+const server = require('server');
+const { get, post } = server.router;
+const { render, json } = server.reply;
+
+// server([
+//   get('/', ctx => 'Hello world!')
+// ]);
+
+
+server({ port: 3000 }, ctx => 'Hello 世界');
