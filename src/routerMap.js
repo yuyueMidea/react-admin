e9cd98bf5236bb29113c322b1f685db7a5d6d40b
苍穹勇敢的心 <18825237023@163.com>
2019-11-26 18:14:50 +0800

    新增【新闻一览】页面，样式调整

--- src/routerMap.js
+++ src/routerMap.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-21 19:52:42
- * @LastEditTime: 2019-11-23 17:59:49
+ * @LastEditTime: 2019-11-26 17:49:04
  * @LastEditors: Please set LastEditors
  */
 // 引入组件----------NotFound---
@@ -14,6 +14,9 @@ import TodoList from './components/TodoList';
 import ItemList from './components/ItemList'
 import XiaoquList from './components/XiaoquList'
 import BookList from './components/BookList'
+import NewsList from './components/NewsList'
+import CompanyList from './components/CompanyList'
+
 
 export default [
   {path: "/home",component: Home,name:'主页', auth:true},
@@ -21,6 +24,8 @@ export default [
   {path: "/itemList",component: ItemList,name:'商品一览页面', auth:true},
   {path: "/xiaoquList",component: XiaoquList,name:'小区一览页面', auth:true},
   {path: "/bookList",component: BookList,name:'小说一览页面',auth:true},
+  {path: "/newsList",component: NewsList,name:'新闻一览页面',auth:true},
+  {path: "/companyList",component: CompanyList,name:'公司一览页面',auth:true},
   {path: "/todoList/", component: TodoList,name:'TodoList',auth:true,
   },
 ];
